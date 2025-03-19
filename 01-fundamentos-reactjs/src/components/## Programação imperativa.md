3d3c773138d209b19ba4567122312110cb838e31
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-03-18 22:03:55 -0300

    opa

---
+++ 01-fundamentos-reactjs/src/components/## Programação imperativa.md
@@ -0,0 +1,20 @@
+## Programação imperativa
+
+O que deve ser feito.
+
+# Receita de Bolo
+
+1. Ligar o forno a 180*;
+2. Abri a porta do forno;
+
+
+
+## Programação Declarativa //preferencia no react
+
+Quais as condições para se ter o resultado final.
+
+# Receita de bolo
+
+1. O forno precisa estar a 180*;
+2. Quando o forno estiver quente, eu posso colocar a massa para assar;
+3. Quando a massa estiver pronta, eu posso retira-la do forno
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,12 +1,53 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
+
+
+// estado = variaveis que eu quero que o componente monitore
+
+
 export function Post({author , publishedAt, content}) {
+   
+    const [comments, setComments]= useState([
+        'Post interessante, hein?!'
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+   
+    const publishedDateFormatted = format(publishedAt,"d 'de' LLLL 'às' HH:mm'h'",{
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix:true,
+    })
+    
+    function handleCreateNewComment() {
+        event.preventDefault()
+        
+        //imutabilidade
+
+        const newCommentText = event.target.comment.value
+        
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+        
+    
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
+        
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -29,15 +70,19 @@ export function Post({author , publishedAt, content}) {
             if(line.type == 'paragraph'){
                 return <p>{line.content}</p>
             } else if (line.type == 'link'){
-                return <p><a href="">{line.content}</a></p>
+                return <p><a href="#">{line.content}</a></p>
             }
-           })}            </div>
+           })}           
+            </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
+                name="comment"
                   placeholder='Deixe um comentário'
+                  value={newCommentText}
+                  onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -46,9 +91,9 @@ export function Post({author , publishedAt, content}) {
             </form>
 
             <div className={styles.commentList}>
-               <Comment />
-               <Comment />
-               <Comment />
+              {comments.map(comment => {
+                return <Comment  content={comment} />
+              })}
             </div>
         </article>
        )
